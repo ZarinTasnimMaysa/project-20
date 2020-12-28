dd3cb25ea06ee671cafd3aeeebeda588e6a467ed
ZarinTasnimMaysa <75082297+ZarinTasnimMaysa@users.noreply.github.com>
2020-12-28 22:38:17 +0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var car , wall;
+var speed, weight;
+
+function setup() {
+  createCanvas(1600,400);
+  
+  speed =random(55,90);
+  weight =random(400,1500);
+  
+   car= createSprite(50, 200, 50, 50);
+   car.velocityX=speed;
+   car.shapecolour=colour(255);
+
+   wall= createSprite(1500, 200, 50,height/2);
+   wall.shapecolour=colour(80,80,80);
+  }
+
+function draw() {
+  background(0);  
+  drawSprites();
+}
